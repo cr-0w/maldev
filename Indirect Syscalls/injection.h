d159c9fd3291b2190d7623e93f3d7c7fcb503e52
crow <crow@crow.rip>
2024-04-08 11:09:19 -0400

    fix

--- Indirect Syscalls/injection.h
+++ Indirect Syscalls/injection.h
@@ -3,7 +3,6 @@
 #include <windows.h>
 
 #define STATUS_SUCCESS (NTSTATUS)0x00000000L
-#define STATUS_SUCCESS (NTSTATUS)0x00000000L
 #define OKAY(MSG, ...) printf("[+] "               MSG "\n", ##__VA_ARGS__)
 #define INFO(MSG, ...) printf("[*] "               MSG "\n", ##__VA_ARGS__)
 #define WARN(MSG, ...) fprintf(stderr, "[-] "      MSG "\n", ##__VA_ARGS__)
